43bd67d8a37f4d8a9ccac5c22ca147f62225439c
Vovchick4 <vovaspodpivas@gmail.com>
2022-07-18 14:19:17 +0300

    FDSF

--- shared/services/bot-telegram.js
+++ shared/services/bot-telegram.js
@@ -1,2 +1,2 @@
 import TelegramBot from "node-telegram-bot-api";
-export const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN_UNIQ, { polling: true })
+export const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN_UNIQ)
